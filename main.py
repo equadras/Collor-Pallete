6ee22f608abc03ed51a69c554dbc44a66e896d0f
equadras <emanueljose.quadras@gmail.com>
2022-08-08 20:12:35 -0300

    segundo commit

--- main.py
+++ main.py
@@ -9,32 +9,89 @@ import cv2
 import extcolors
 from colormap import rgb2hex
 
-input_name = 'foto.jpeg'
-output_width = 900                 
-img = Image.open(input_name)
-wpercent = (output_width/float(img.size[0]))
-hsize = int((float(img.size[1])*float(wpercent)))
-img = img.resize((output_width,hsize), Image.ANTIALIAS)
-
-resize_name = 'resize_' + input_name 
-img.save(resize_name)                
-
-plt.figure(figsize=(9, 9))
-img_url = resize_name
-img = plt.imread(img_url)
-plt.imshow(img)
-plt.axis('off')
-plt.show()
 
 
 
+def exact_color(input_image, resize, tolerance, zoom):
+    #background
+    bg = 'bg.png'
+    fig, ax = plt.subplots(figsize=(192,108),dpi=10)
+    fig.set_facecolor('white')
+    plt.savefig(bg)
+    plt.close(fig)
+    
+    #resize
+    output_width = resize
+    img = Image.open(input_image)
+    if img.size[0] >= resize:
+        wpercent = (output_width/float(img.size[0]))
+        hsize = int((float(img.size[1])*float(wpercent)))
+        img = img.resize((output_width,hsize), Image.ANTIALIAS)
+        resize_name = 'resize_'+ input_image
+        img.save(resize_name)
+    else:
+        resize_name = input_image
+    
+    #crate dataframe
+    img_url = resize_name
+    colors_x = extcolors.extract_from_path(img_url, tolerance = tolerance, limit = 13)
+    df_color = color_to_df(colors_x)
+    
+    #annotate text
+    list_color = list(df_color['c_code'])
+    list_precent = [int(i) for i in list(df_color['occurence'])]
+    text_c = [c + ' ' + str(round(p*100/sum(list_precent),1)) +'%' for c, p in zip(list_color, list_precent)]
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(160,120), dpi = 10)
+    
+    #donut plot
+    wedges, text = ax1.pie(list_precent,
+                           labels= text_c,
+                           labeldistance= 0.80,
+                           colors = list_color,
+                           textprops={'fontsize': 100, 'color':'black'})
+    plt.setp(wedges, width=0.3)
+    
+    #color palette
+    x_posi, y_posi, y_posi2 = 160, -170, -180
+    for c in list_color:
+        if list_color.index(c) <= 5:
+            y_posi += 180
+            rect = patches.Rectangle((x_posi, y_posi), 360, 160, facecolor = c)
+            ax2.add_patch(rect)
+            ax2.text(x = x_posi+400, y = y_posi+100, s = c, fontdict={'fontsize': 150})
+        else:
+            y_posi2 += 180
+            rect = patches.Rectangle((x_posi + 1000, y_posi2), 360, 160, facecolor = c)
+            ax2.add_artist(rect)
+            ax2.text(x = x_posi+1400, y = y_posi2+100, s = c, fontdict={'fontsize': 150})
 
+        #add image in the center of donut plot
+    img = mpimg.imread(resize_name)
+    imagebox = OffsetImage(img, zoom=zoom)
+    ab = AnnotationBbox(imagebox, (1, 1))
+    ax1.add_artist(ab)
 
+    fig.set_facecolor('white')
+    ax2.axis('off')
+    bg = plt.imread('bg.png')
+    plt.imshow(bg)       
+    plt.tight_layout()
+    return plt.show()
 
+def color_to_df(input):
+    colors_pre_list = str(input).replace('([(','').split(', (')[0:-1]
+    df_rgb = [i.split('), ')[0] + ')' for i in colors_pre_list]
+    df_percent = [i.split('), ')[1].replace(')','') for i in colors_pre_list]
+    
+    #convert RGB to HEX code
+    df_color_up = [rgb2hex(int(i.split(", ")[0].replace("(","")),
+                          int(i.split(", ")[1]),
+                          int(i.split(", ")[2].replace(")",""))) for i in df_rgb]
+    
+    df = pd.DataFrame(zip(df_color_up, df_percent), columns = ['c_code','occurence'])
+    return df
 
+colors_x = extcolors.extract_from_path('foto.jpeg', tolerance = 20, limit = 18)
+colors_x
 
-
-
-
-
-
+exact_color('foto.jpeg', 720, 20, 1)
